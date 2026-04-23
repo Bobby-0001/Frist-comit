f83f26b94a063558a31130fc5eee340e5cebaebb
Your Name <your@email.com>
2026-04-23 20:59:41 +0530

    fix deployment

--- myreact-app/src/components/Header.jsx
+++ myreact-app/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header style={{ textAlign: "center", margin: "20px" }}>
       <h1>Todo List Project</h1>
-      <p>Manage your daily tasks easily</p>
+      <p>vayuz</p>
     </header>
   );
 }
--- myreact-app/src/components/TodoList.jsx
+++ myreact-app/src/components/TodoList.jsx
@@ -1,20 +1,20 @@
-// // components/TodoList.jsx
-// import React from "react";
+// components/TodoList.jsx
+import React from "react";
 
-// export default function TodoList({ todos, deleteTodo }) {
-//   return (
-//     <ul style={{ listStyle: "none", padding: 0, textAlign: "center" }}>
-//       {todos.map((todo, index) => (
-//         <li key={index} style={{ margin: "10px" }}>
-//           {todo}
-//           <button
-//             onClick={() => deleteTodo(index)}
-//             style={{ marginLeft: "10px" }}
-//           >
-//             Delete
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
+export default function TodoList({ todos, deleteTodo }) {
+  return (
+    <ul style={{ listStyle: "none", padding: 0, textAlign: "center" }}>
+      {todos.map((todo, index) => (
+        <li key={index} style={{ margin: "10px" }}>
+          {todo}
+          <button
+            onClick={() => deleteTodo(index)}
+            style={{ marginLeft: "10px" }}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
